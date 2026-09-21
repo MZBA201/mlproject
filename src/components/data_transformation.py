47c98d9b775ed9e3851170815872d36fada97f37
MZBA201 <akbar.zain@yahoo.com>
2026-09-21 21:56:27 +1000

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -48,7 +48,7 @@ class DataTransformation:
         except Exception as e:
             raise CustomException(e, sys)
 
-    def initiate_data_trasformation(self, train_path, test_path):
+    def initiate_data_transformation(self, train_path, test_path):
         try:
             train_df = pd.read_csv(train_path)
             test_df = pd.read_csv(test_path)
